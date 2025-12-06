99a4363a294d50d6d3aa60e9b0fa4d320617938f
Gaurav Regmi <gauravregmi@Mac.ht.home>
2025-12-06 17:31:58 -0600

    fpims initialization

---
+++ src/main/java/com/pantry/ims/PantryImsApplication.java
@@ -0,0 +1,13 @@
+package com.pantry.ims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantryImsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pantry/ims/PantryImsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pantry.ims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
