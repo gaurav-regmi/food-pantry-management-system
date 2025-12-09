b938275cbe86743f5efe191762c2dbd4cfa06c29
Sambhav196 <sambhav.siwakoti196@gmail.com>
2025-12-09 16:21:59 -0600

    customer modify and delete api created

--- src/main/java/com/pantry/ims/controller/CustomerController.java
+++ src/main/java/com/pantry/ims/controller/CustomerController.java
@@ -1,16 +1,17 @@
 package com.pantry.ims.controller;
 
-import com.pantry.ims.dto.CustomerRequestDTO;
-import com.pantry.ims.dto.CustomerResponseDTO;
+import com.pantry.ims.dto.*;
 import com.pantry.ims.entity.CustomerEntity;
 import com.pantry.ims.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("customer")
@@ -36,7 +37,7 @@ public class CustomerController {
 
         List<CustomerResponseDTO> customerResponseDTOList = new ArrayList<>();
 
-        for(CustomerEntity customerEntity: customerEntityList){
+        for (CustomerEntity customerEntity : customerEntityList) {
             CustomerResponseDTO customerResponseDTO = new CustomerResponseDTO();
             customerResponseDTO.setId(customerEntity.getId());
             customerResponseDTO.setName(customerEntity.getName());
@@ -47,5 +48,56 @@ public class CustomerController {
 
         return ResponseEntity.ok(customerResponseDTOList);
     }
+
+    @PutMapping(value = "update", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateCustomer(@RequestBody CustomerModifyDTO modifyDTO) {
+        try {
+            Optional<CustomerEntity> optionalCustomerEntity = customerRepository.findById(modifyDTO.getId());
+
+            CustomerResponseDTO responseDTO = new CustomerResponseDTO();
+
+            if (optionalCustomerEntity.isEmpty()) {
+                //throw a not found message;
+                responseDTO.setMessage("Customer not found");
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDTO);
+            }
+
+            CustomerEntity customerEntity = optionalCustomerEntity.get();
+            customerEntity.setName(modifyDTO.getName());
+            customerEntity.setAddress(modifyDTO.getAddress());
+
+            CustomerEntity updatedCustomer = customerRepository.save(customerEntity);
+
+
+            responseDTO.setName(updatedCustomer.getName());
+            responseDTO.setAddress(updatedCustomer.getAddress());
+            responseDTO.setMessage("Customer updated successfully");
+
+            return ResponseEntity.ok(responseDTO);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Error");
+        }
+
+
+    }
+
+    @DeleteMapping(value = "delete")
+    public CustomerDeleteResponseDTO deleteCustomer(@RequestBody CustomerDeleteDTO deleteDTO) {
+        Optional<CustomerEntity> optionalCustomerEntity = customerRepository.findById(deleteDTO.getId());
+        if (optionalCustomerEntity.isEmpty()) {
+            CustomerDeleteResponseDTO customerDeleteResponseDTO = new CustomerDeleteResponseDTO();
+            customerDeleteResponseDTO.setMessage("Customer not found");
+            return customerDeleteResponseDTO;
+
+        }
+        CustomerEntity customer = optionalCustomerEntity.get();
+        customerRepository.delete(customer);
+
+        CustomerDeleteResponseDTO customerDeleteResponseDTO = new CustomerDeleteResponseDTO();
+        customerDeleteResponseDTO.setMessage("Customer deleted successfully");
+        return customerDeleteResponseDTO;
+
+    }
+
 
 }
