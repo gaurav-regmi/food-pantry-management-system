24f36de3db01d24a711062e2fd3ced3989895ccf
Sambhav196 <sambhav.siwakoti196@gmail.com>
2025-12-25 15:30:18 -0600

    Seller info created

--- src/main/java/com/pantry/ims/controller/SellerController.java
+++ src/main/java/com/pantry/ims/controller/SellerController.java
@@ -3,13 +3,14 @@ package com.pantry.ims.controller;
 import com.pantry.ims.dto.CustomerRequestDTO;
 import com.pantry.ims.dto.CustomerResponseDTO;
 import com.pantry.ims.dto.SellerRequestDTO;
+import com.pantry.ims.dto.SellerResponseDTO;
 import com.pantry.ims.entity.SellerEntity;
 import com.pantry.ims.repository.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("seller")
@@ -24,6 +25,28 @@ public class SellerController {
         sellerRepository.save(sellerEntity);
         return "Your seller information is saved.";
     }
+
+    @GetMapping(value = "info")
+    public List<SellerResponseDTO> DisplaySellerInfo(){
+        List<SellerEntity> sellerEntityList = sellerRepository.findAll();
+
+        List<SellerResponseDTO> sellerResponseDTOList = new ArrayList<>();
+
+        for(SellerEntity sellerEntity : sellerEntityList)
+        {
+            SellerResponseDTO sellerResponseDTO = new SellerResponseDTO();
+            sellerResponseDTO.setId(sellerEntity.getId());
+            sellerResponseDTO.setName(sellerEntity.getName());
+            sellerResponseDTO.setAddress(sellerEntity.getAddress());
+
+            sellerResponseDTOList.add(sellerResponseDTO);
+
+        }
+        return sellerResponseDTOList;
+
+    }
+
+
 
 
 
